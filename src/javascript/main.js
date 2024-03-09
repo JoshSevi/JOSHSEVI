839f3b3c22f0cab2fd2b77d300076a69945e114f
Joshua Sevilla <jSevilla@mcm.edu.ph>
2024-03-10 03:27:58 +0800

    card animation

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -85,5 +85,30 @@ document.addEventListener('DOMContentLoaded', function() {
             document.getElementById('menuToggle').style.visibility = 'visible';
         }, 3000);
     }
-});
 
+
+    const cards = document.querySelectorAll('.card');
+    cards.forEach((card, index) => {
+        const offsetTop = 20 + index * 50;
+        card.style.paddingTop = `${offsetTop}px`;
+
+        if (index === cards.length - 1) {
+            return;
+        }
+
+        const toScale = 1 - (cards.length - 1 - index) * 0.06;
+        const nextCard = cards[index + 1];
+        const cardInner = card.querySelector('.card-body');
+
+        gsap.to(cardInner, {
+            scrollTrigger: {
+                trigger: nextCard,
+                start: `top+=${offsetTop} bottom`,
+                end: `bottom bottom-=${card.clientHeight}`, // Adjusted end position
+                scrub: true
+            },
+            scale: toScale,
+        });
+    });
+
+});
